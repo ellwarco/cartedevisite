577a4c04cf4a995a818d28ffa7aa1031490d0b8a
Sergey <s.shmyrev@laposte.net>
2017-05-30 13:16:56 +0200

    Initial Commit

---
+++ CarteDeVisite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarteDeVisite
+//
+//  Created by Sergey Shmyrev on 30/05/2017.
+//  Copyright © 2017 Sergey Shmyrev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
